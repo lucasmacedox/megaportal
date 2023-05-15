c84089a50cb1069b6d92d56b43af281580aba4d7
Lucas Macedo <lucas@byfrost.net.br>
2023-05-14 22:50:41 -0300

    Lesson 8 - Changer Slideshow
    - Application of images and automatic movement of the slideshow.
    - Changed color main menu.
    - Changed menu names.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,39 @@
+// Global variable to track the current slide
 var slideItem = 0;
-window.onload = function () {
-    setInterval(passarSlide, 2000);
+// Variable to store the interval for automatic slide transition
+var slideInterval;
 
+/**
+ * Function executed when the page is loaded.
+ * Initializes the slide and sets the slide sizes.
+ */
+window.onload = function () {
+    startSlideInterval();
+
+    // Get the width of the slideshow element
     var slidewidth = document.getElementById("slideshow").offsetWidth;
+    // Set the width of each slide based on the slideshow width
     var objs = document.getElementsByClassName("slide");
     for (var i = 0; i < objs.length; i++) {
         objs[i].style.width = slidewidth + "px";
     }
+};
+
+/**
+ * Starts the interval for automatic slide transition.
+ */
+function startSlideInterval() {
+    slideInterval = setInterval(function () {
+        slidePass(); // Move to the next slide
+        updateSlideCircle(); // Update the active slide indicator
+    }, 2000); // 2-second interval for automatic transition
 }
-function passarSlide() {
+
+/**
+ * Moves to the next slide.
+ */
+function slidePass() {
+    // Get the width of the slideshow element
     var slidewidth = document.getElementById("slideshow").offsetWidth;
 
     if (slideItem >= 3) {
@@ -17,22 +42,51 @@ function passarSlide() {
         slideItem++;
     }
 
-    document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-" + (slidewidth * slideItem) + "px";
+    // Apply negative left margin to show the next slide
+    document.getElementsByClassName("slideshow-container")[0].style.marginLeft = "-" + (slidewidth * slideItem) + "px";
 }
-function mudarSlide(pos) {
+
+/**
+ * Changes the slide based on the received index.
+ * @param {number} pos - Index of the slide to display.
+ */
+function changerSlide(pos) {
     slideItem = pos;
+    updateSlideCircle(); // Update the active slide indicator
+
+    // Get the width of the slideshow element
     var slidewidth = document.getElementById("slideshow").offsetWidth;
-    document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-" + (slidewidth * slideItem) + "px";
+    // Apply negative left margin to show the selected slide
+    document.getElementsByClassName("slideshow-container")[0].style.marginLeft = "-" + (slidewidth * slideItem) + "px";
 }
 
-function toggleMenu() {
+/**
+ * Updates the active slide indicator.
+ */
+function updateSlideCircle() {
+    // Select all elements with the 'slide-circle' class
+    var slideCircles = document.querySelectorAll('.slide-circle');
 
+    // Remove the 'active' class from all elements
+    slideCircles.forEach(function (circle, index) {
+        if (index === slideItem) {
+            circle.classList.add('active'); // Add the 'active' class to the current slide
+        } else {
+            circle.classList.remove('active'); // Remove the 'active' class from other slides
+        }
+    });
+}
+
+/**
+ * Toggles the menu display.
+ */
+function toggleMenu() {
     var menu = document.getElementById("menu");
 
+    // Check if the menu is hidden or visible
     if (menu.style.display == 'none' || menu.style.display == '') {
-        menu.style.display = "block";
+        menu.style.display = "block"; // Display the menu
     } else {
-        menu.style.display = "none";
+        menu.style.display = "none"; // Hide the menu
     }
-
 }
